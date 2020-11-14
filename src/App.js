e37d4c179000d82d219f13e4de4ef11b823c9b8d
Олександр Тупчієнко <tupchienkoos@gmail.com>
2020-11-14 14:06:06 +0400

    Register logic in register.js

--- src/App.js
+++ src/App.js
@@ -6,52 +6,11 @@ class App extends React.Component {
     super();
     this.state = {
       confirmed: false,
-      values: {
-        name: "",
-        email: "",
-        password: "",
-        termsNPolicy: false,
-      },
-      hover: { termsNPolicy: false },
     };
   }
 
-  onSubmit = (event) => {
-    this.setState({ confirmed: true });
-  };
-
-  onMouseEnter = (event) => {
-    this.setState({ hover: { termsNPolicy: true } });
-  };
-  onMouseLeave = (event) => {
-    this.setState({ hover: { termsNPolicy: false } });
-  };
-
-  onChangeInput = (event) => {
-    const { values } = this.state;
-    const { name, value } = event.target;
-    const updatedValues = { ...values };
-    this.setState({
-      values: {
-        ...updatedValues,
-        [name]: name === "termsNPolicy" ? event.target.checked : value,
-      },
-    });
-  };
-
   render() {
-    return (
-      !this.state.confirmed && (
-        <Register
-          values={this.state.values}
-          termsNPolicyHover={this.state.hover.termsNPolicy}
-          onSubmit={this.onSubmit}
-          onMouseEnter={this.onMouseEnter}
-          onMouseLeave={this.onMouseLeave}
-          onChangeInput={this.onChangeInput}
-        />
-      )
-    );
+    return !this.state.confirmed && <Register />;
   }
 }
 
--- src/Register.js
+++ src/Register.js
@@ -3,83 +3,113 @@ import { InputField } from "./input-field";
 import { CheckBox } from "./check-box";
 import { Button } from "./button";
 
-export const Register = ({
-  confirmed,
-  values: { name, email, password, termsNPolicy },
-  termsNPolicyHover,
-  onSubmit,
-  onMouseEnter,
-  onMouseLeave,
-  onChangeInput,
-}) => {
-  //const { name, email, password, termsNPolicy } = { values };
-  return (
-    <div className="gray-bg">
-      <div className="middle-box text-center loginscreen   animated fadeInDown">
-        <div>
+class Register extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      values: {
+        name: "",
+        email: "",
+        password: "",
+        termsNPolicy: false,
+      },
+      hover: { termsNPolicy: false },
+    };
+  }
+
+  onSubmit = (event) => {
+    this.setState({ confirmed: true });
+  };
+
+  onMouseEnter = (event) => {
+    this.setState({ hover: { termsNPolicy: true } });
+  };
+  onMouseLeave = (event) => {
+    this.setState({ hover: { termsNPolicy: false } });
+  };
+
+  onChangeInput = (event) => {
+    const { values } = this.state;
+    const { name, value } = event.target;
+    const updatedValues = { ...values };
+    this.setState({
+      values: {
+        ...updatedValues,
+        [name]: name === "termsNPolicy" ? event.target.checked : value,
+      },
+    });
+  };
+
+  render() {
+    const { name, email, password, termsNPolicy } = this.state.values;
+    return (
+      <div className="gray-bg">
+        <div className="middle-box text-center loginscreen   animated fadeInDown">
           <div>
-            <h1 className="logo-name">IN+</h1>
-          </div>
-          <h3>Register to IN+</h3>
-          <p>Create account to see it in action.</p>
-          <form className="m-t">
-            {/*role="form" action="login.html" */}
-            <InputField
-              id="Name"
-              name="name"
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChangeInput={onChangeInput}
-            />
-            <InputField
-              id="Email"
-              name="email"
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChangeInput={onChangeInput}
-            />
-            <InputField
-              id="Password"
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={password}
-              onChangeInput={onChangeInput}
-            />
-            <CheckBox
-              id="TermsNPolicy"
-              name="termsNPolicy"
-              value={termsNPolicy}
-              label="Agree the terms and policy"
-              hover={termsNPolicyHover}
-              onChangeInput={onChangeInput}
-              onMouseEnter={onMouseEnter}
-              onMouseLeave={onMouseLeave}
-            />
-            <Button
-              type="submit"
-              className="btn btn-primary block full-width m-b"
-              label="Register"
-              onSubmit={onSubmit}
-            />
-            <p className="text-muted text-center">
-              <small>Already have an account?</small>
+            <div>
+              <h1 className="logo-name">IN+</h1>
+            </div>
+            <h3>Register to IN+</h3>
+            <p>Create account to see it in action.</p>
+            <form className="m-t">
+              {/*role="form" action="login.html" */}
+              <InputField
+                id="Name"
+                name="name"
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChangeInput={this.onChangeInput}
+              />
+              <InputField
+                id="Email"
+                name="email"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChangeInput={this.onChangeInput}
+              />
+              <InputField
+                id="Password"
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChangeInput={this.onChangeInput}
+              />
+              <CheckBox
+                id="TermsNPolicy"
+                name="termsNPolicy"
+                value={termsNPolicy}
+                label="Agree the terms and policy"
+                hover={this.state.hover.termsNPolicy}
+                onChangeInput={this.onChangeInput}
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+              />
+              <Button
+                type="submit"
+                className="btn btn-primary block full-width m-b"
+                label="Register"
+                onSubmit={this.onSubmit}
+              />
+              <p className="text-muted text-center">
+                <small>Already have an account?</small>
+              </p>
+              <a className="btn btn-sm btn-white btn-block" href="login.html">
+                Login
+              </a>
+            </form>
+            <p className="m-t">
+              <small>
+                Inspinia we app framework base on Bootstrap 3 &copy; 2014
+              </small>
             </p>
-            <a className="btn btn-sm btn-white btn-block" href="login.html">
-              Login
-            </a>
-          </form>
-          <p className="m-t">
-            <small>
-              Inspinia we app framework base on Bootstrap 3 &copy; 2014
-            </small>
-          </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Register;
