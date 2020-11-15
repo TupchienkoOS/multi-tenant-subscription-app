8741888886658cfda3e7efcfa03a55b603e96ff1
Олександр Тупчієнко <tupchienkoos@gmail.com>
2020-11-15 09:15:57 +0400

    create new folder for profile components

--- src/profile.js
+++ src/profile.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { NavBarSide } from "./nav-bar-side";
-import { NavBarTop } from "./nav-bar-top";
-import { PageHeading } from "./page-heading";
-import { PageContent } from "./page-content";
-import { PageFooter } from "./page-footer";
+import { NavBarSide } from "./profile/nav-bar-side";
+import { NavBarTop } from "./profile/nav-bar-top";
+import { PageHeading } from "./profile/page-heading";
+import { PageContent } from "./profile/page-content";
+import { PageFooter } from "./profile/page-footer";
 
 class Profile extends React.Component {
   constructor(props) {
