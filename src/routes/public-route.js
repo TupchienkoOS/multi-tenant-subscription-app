532245578ce909ce81a9439cf21929d5fb7bf7cf
Олександр Тупчієнко <tupchienkoos@gmail.com>
2021-01-27 20:52:09 +0400

    refactoring publick routes

--- src/routes/public-route.js
+++ src/routes/public-route.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { Route, Redirect, matchPath } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Cookies from "js-cookie";
 import { roles } from "../data/roles";
 import DbApi from "../data/dbApi";
 
-//Розібратись шоб ініціалізація функцій була перед їх викристанням
 export const PublicRoute = (props) => {
   const user = Cookies.get("user");
   const company = Cookies.get("company");
 
   console.log("public route", props);
 
+  const role = props.computedMatch.params.role;
+
   const getDefaultPath = () => {
     let defaultPath;
-    if (isUser && user) {
+    if (role === "user" && user) {
       defaultPath = roles.filter(
         (role) => role.id === DbApi.getUserById(user).role
       )[0].defaultPath;
-    } else if (isCompany && company) {
+    } else if (role === "company" && company) {
       defaultPath = roles.filter(
         (role) => role.id === DbApi.getCompanyById(company).role
       )[0].defaultPath;
@@ -36,22 +37,9 @@ export const PublicRoute = (props) => {
     return props.location.state.from.pathname;
   };
 
-  const isCompany = matchPath(props.location.pathname, {
-    path: "/company",
-    exact: false,
-    strict: true,
-  });
-  const isUser = matchPath(props.location.pathname, {
-    path: "/user",
-    exact: false,
-    strict: true,
-  });
-
-  console.log("public route", isCompany, isUser);
-
   const isLogin = () => {
-    if (isUser && user) return true;
-    else if (isCompany && company) return true;
+    if (role === "user" && user) return true;
+    else if (role === "company" && company) return true;
     else return false;
   };
 
